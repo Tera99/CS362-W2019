7f1bff73cd9396409b772649c759765d56259326
Tera Schaller <tschaller722@gmail.com>
2019-02-06 15:42:09 -0600

    problems with strings2

--- dominion/unittest1.c
+++ dominion/unittest1.c
@@ -5,7 +5,7 @@ Unit tests for dominion.c
 **************************************/
 #include "dominion.h"
 #include "dominion_helpers.h"
-#include <string>
+#include "string.h"
 #include <stdio.h>
 #include <assert.h>
 #include "rngs.h"

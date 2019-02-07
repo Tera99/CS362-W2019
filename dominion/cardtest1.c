a099028d46c5f81d9796115d47b505e156ceb5c1
Tera Schaller <tschaller722@gmail.com>
2019-02-07 13:14:00 -0600

    working on cardtest1.c, getting cardEffect function working

--- dominion/cardtest1.c
+++ dominion/cardtest1.c
@@ -6,6 +6,8 @@ Unit tests for dominion.c
 /*************************************
 cardtest1.c
 testing adventurerEffect function
+REFERENCES
+Lecture material: https://oregonstate.instructure.com/courses/1706563/files/73152166?module_item_id=18416505
 **************************************/
 #include "dominion.h"
 #include "dominion_helpers.h"
@@ -22,6 +24,16 @@ int main(){
   struct gameState myState;
   //clear the struct with memset
   memset(&myState, 0, sizeof(myState));
+
+  //need a valid game in progress to call adventurerEffect
+  //or else it segfaults
+  int numPlayers = 2;
+  int k[10] = {adventurer, embargo, village, minion, mine,
+    cutpurse,sea_hag, tribute, smithy, council_room};
+  int seed = 1000;
+
+  // initialize a game state and player cards
+  initializeGame(numPlayers, k, seed, &myState);
 
   //call adventurerEffect, should return zero
   result = adventurerEffect(&myState);

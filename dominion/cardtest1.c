79a7b3780d66701f1dc7c39ce54d2f471506bf90
Tera Schaller <tschaller722@gmail.com>
2019-02-07 11:33:39 -0600

    begin working on smithEffect unit test

--- dominion/cardtest1.c
+++ dominion/cardtest1.c
@@ -3,6 +3,10 @@ Terezie (Tera) Schaller
 Assignment 3
 Unit tests for dominion.c
 **************************************/
+/*************************************
+cardtest1.c
+testing adventurerEffect function
+**************************************/
 #include "dominion.h"
 #include "dominion_helpers.h"
 #include <string.h>
@@ -11,8 +15,19 @@ Unit tests for dominion.c
 #include "rngs.h"
 int main(){
   printf("~~~~~Begining Card Test 1~~~~~\n");
+  printf("~~~~~Testing adventurerEffect function~~~~~\n");
+
+  //initialize a blank game state
+  struct gameState myState;
+  //clear the struct with memset
+  memset(&myState, 0, sizeof(myState));
+
+  //call adventurerEffect, should return zero
+  int result = adventurerEffect(&myState);
+  printf("Test result: %d\n", result);
 
 
+  printf("~~~~~End testing adventurerEffect function~~~~~\n");
   printf("~~~~~End Unit Test 1~~~~~\n");
   return 0;
 }

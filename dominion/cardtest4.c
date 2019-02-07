322a29ae3ed348ec90087fd791fc7fddbf2aebbb
Tera Schaller <tschaller722@gmail.com>
2019-02-07 13:47:59 -0600

    cardtest4.c

--- dominion/cardtest4.c
+++ dominion/cardtest4.c
@@ -11,7 +11,29 @@ Unit tests for dominion.c
 #include "rngs.h"
 int main(){
   printf("~~~~~Begining Card Test 4~~~~~\n");
+  printf("~~~~~Testing sea_hagEffect function~~~~~\n");
 
+  int result;
+  struct gameState myState;
+  //clear the struct with memset
+  memset(&myState, 0, sizeof(myState));
+
+  //need a valid game in progress to call adventurerEffect
+  //or else it segfaults
+  int numPlayers = 2;
+  int k[10] = {adventurer, embargo, village, minion, mine,
+    cutpurse,sea_hag, tribute, smithy, council_room};
+  int seed = 1000;
+
+  // initialize a game state and player cards
+  initializeGame(numPlayers, k, seed, &myState);
+
+  //call villageEffect, should return zero
+  result = sea_hagEffect(&myState);
+  printf("Test result: %d\n", result);
+
+
+  printf("~~~~~End testing sea_hagEffect function~~~~~\n");
   printf("~~~~~End Card Test 4~~~~~\n");
   return 0;
 }

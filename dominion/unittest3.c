4e16f1cb736a838a8c6ed38131bb43a863eb91fa
Tera Schaller <tschaller722@gmail.com>
2019-02-08 15:55:31 -0600

    unittest3.c - more tests

--- dominion/unittest3.c
+++ dominion/unittest3.c
@@ -81,6 +81,42 @@ int main(){
     printf("FAILED\n");
   }
 
+  //test hand + discard + decks
+  //add cards to all three at once
+  //clear game state
+  memset(&myState, 0, sizeof(myState));
+  //add to hand
+  myState.hand[myPlayer][0] = estate;
+  myState.hand[myPlayer][1] = duchy;
+  myState.hand[myPlayer][2] = province;
+  myState.hand[myPlayer][3] = great_hall;
+  myState.hand[myPlayer][4] = curse;
+  myState.handCount[myPlayer] = 5;
+  //add to discard
+  myState.discard[myPlayer][0] = estate;
+  myState.discard[myPlayer][1] = duchy;
+  myState.discard[myPlayer][2] = province;
+  myState.discard[myPlayer][3] = great_hall;
+  myState.discard[myPlayer][4] = curse;
+  myState.discardCount[myPlayer] = 5;
+  //add to deck
+  myState.deck[myPlayer][0] = estate;
+  myState.deck[myPlayer][1] = duchy;
+  myState.deck[myPlayer][2] = province;
+  myState.deck[myPlayer][3] = great_hall;
+  myState.deck[myPlayer][4] = curse;
+  myState.deckCount[myPlayer] = 5;
+  //print result
+  int result5 = scoreFor(myPlayer, &myState);
+  printf("Test 5 - hand + discard + deck, expected: 30 - result: %d :", result5);
+  if (result4 == 30){
+    printf("PASSED\n");
+  } else {
+    printf("FAILED\n");
+  }
+
+
+
   printf("~~~~~ End testing scoreFor function ~~~~~\n");
   printf("~~~~~End Unit Test 3~~~~~\n");
   return 0;

7bb6c3dce0d922997dbb5d13e274a5e373b86e79
Tera Schaller <tschaller722@gmail.com>
2019-02-08 09:41:31 -0600

    unittest2.c more tests

--- dominion/unittest2.c
+++ dominion/unittest2.c
@@ -18,12 +18,33 @@ int main(){
   printf("~~~~~Testing supplyCount function~~~~~\n");
   //initialize variables
   int myCard = 0;
+  int myCard1 = 1;
+  int myCard2 = 2;
   struct gameState myState;
   myState.supplyCount[myCard] = 0;
-  int result;
+  int result, result1, result2;
   //call supplyCount function
   result = supplyCount(myCard, &myState);
-  printf("The test result is: %d\n", result);
+
+  printf("Test 0 result is: %d\n", result);
+
+  //test 2 - add some cards to the deck
+  myState.supplyCount[myCard1] = 25;
+  mySate.supplyCount[myCard2] = 100;
+
+  result1 = supplyCount(myCard1, &myState);
+  result2 = supplyCount(myCard2, &myState);
+
+  printf("Test 1 result: %d\n", result1);
+  printf("Test 2 result: %d\n", result2);
+
+  //subtract cards from the deck
+
+  //negative number of cards??
+
+  //extremely large number of a card
+
+  //check the last type of card
 
   printf("~~~~~End testing supplyCount function~~~~~\n");
   printf("~~~~~End Unit Test 2~~~~~\n");

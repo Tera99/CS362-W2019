41f48160a3c341deea1ddea10d1977c801ea7bac
Tera Schaller <tschaller722@gmail.com>
2019-02-09 12:41:27 -0600

    cardtest4.c - sea_hag

--- dominion/cardtest4.c
+++ dominion/cardtest4.c
@@ -53,6 +53,41 @@ int main(){
     printf("FAILED\n");
   }
 
+  //test drawing a card
+  //hand - difference should be zero
+  int currentPlayer = whoseTurn(&myState2);
+  int result2 = myState.handCount[currentPlayer];
+  int result3 = myState2.handCount[currentPlayer];
+  printf("Test 2 - num cards in hand: ");
+  if (result3 == result2){
+    printf("PASSED\n");
+  } else {
+    printf("FAILED\n");
+  }
+
+  //discard pile
+  int result4 = myState.discardCount[currentPlayer];
+  int result5 = myState2.discardCount[currentPlayer];
+  printf("Test 3 - num cards in discard pile: ");
+  if (result5 == result4){
+    printf("PASSED\n");
+  } else {
+    printf("FAILED\n");
+  }
+
+  //deck
+  int result6 = myState.deckCount[currentPlayer];
+  int result7 = myState2.deckCount[currentPlayer];
+  printf("Test 4 - num cards in deck: ");
+  if (result7 == result6){
+    printf("PASSED\n");
+  } else {
+    printf("FAILED\n");
+  }
+
+  //test other player 
+
+
 
 
   printf("~~~~~End testing sea_hagEffect function~~~~~\n");

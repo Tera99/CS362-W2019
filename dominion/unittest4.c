b6883050ce2644e34edfde48ca107e80ef360854
Tera Schaller <tschaller722@gmail.com>
2019-02-09 06:51:25 -0600

    unittest4.c

--- dominion/unittest4.c
+++ dominion/unittest4.c
@@ -27,6 +27,21 @@ int main(){
   int result = updateCoins(myPlayer, &myState, myBonus);
   printf("Test results: %d\n", result);
 
+  //coins in hand left over from previous turn
+  myState.coins = 10;
+  int result2 = updateCoins(myPlayer, &myState, myBonus);
+  printf("Test results: %d\n", result2);
+
+  //test copper
+
+  //test silver
+
+  //test gold
+
+  //test bonus
+
+  //test copper + silver + gold + bonus
+
   printf("~~~~~End testing updateCoins function~~~~~\n");
   printf("~~~~~End Unit Test 4~~~~~\n");
   return 0;
